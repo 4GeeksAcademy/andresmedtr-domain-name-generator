77d8cb336b73c034685731781f0cb9b24a5aab26
Andres Medina <123752360+andresmedtr@users.noreply.github.com>
2023-05-02 14:16:15 +0000

    Finished with addons

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ let adj = ["great", "big"];
 let noun = ["jogger", "racoon", "lastofus"];
 let ext = [".net", ".us", ".io", ".etc", ".com"];
 
-noun[2].replace("us");
-
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < ext.length; l++) {
-            if (noun[k].endsWith("us")) {
-            }
-            console.log(pronoun[i] + adj[j] + noun[k] + ext[l]);
+      for (let l = 0; l < ext.length; l++) {
+        if (noun[k].endsWith("us")) {
+          let newNoun = noun[k].replace("us", "");
+          console.log(pronoun[i] + adj[j] + newNoun + ext[1]);
+        } else {
+          console.log(pronoun[i] + adj[j] + noun[k] + ext[l]);
         }
+      }
     }
   }
 }
